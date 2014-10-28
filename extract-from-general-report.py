4314d106bdc95c73fbf7ea8bd84755985689e15b
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-10-28 11:20:01 -0500

    Started reworking the main function.

--- extract-from-general-report.py
+++ extract-from-general-report.py
@@ -198,7 +198,7 @@ def parse_general_report(input_file_path):
             if row_type == "cycle":
                 cycle_id = int(cols[colnum(column_dict[row_type]['Cycle ID'])])
                 assert cycle_id not in cycle_dict.keys()
-                print "Extracting cycle #",cycle_id
+                #print "Extracting cycle #",cycle_id
                 cycle_dict[cycle_id] = {}
                 capacity_charge = cols[colnum(column_dict[row_type]['Cycle charge capacity [mAh]'])]
                 cycle_dict[cycle_id]['Cycle charge capacity [mAh]'] = capacity_charge
@@ -287,104 +287,74 @@ def main():
         os.mkdir(folder_name)
     full_basename = os.path.join(folder_name, basename_no_extension)
 
-    # TODO: split this off into a separate function.
-    with open(input_file_path) as general_report:
-        column_dict = infer_input_file_format(general_report.readline())
-        # Try to infer the mass.
-        # TODO: separate this out to make it less messy.
-        # TODO: restructure this to avoid the duplication of later code.
+    column_dict = infer_input_file_format(general_report.readline())
+    header_comment_character = '#'
+    output_file_extension = '.dat'
+    cycle_summary_file = open(full_basename + "_all_cycle_summary" + output_file_extension, 'w')
+    grace_output_file = open(full_basename + "_grace_ascii.dat", 'w')
+    origin_output_file = open(full_basename + "_origin_columnar.csv", 'w')
+    if mass_g:
+        #TODO: avoid this gross duplication of code.
+        cycle_summary_file.write(header_comment_character + "CycleID" + output_delimiter + "charge capacity [mAh/g]" + output_delimiter + "discharge capacity [mAh/g]\n")
+    else:
+        cycle_summary_file.write(header_comment_character + "CycleID" + output_delimiter + "charge capacity [mAh]" + output_delimiter + "discharge capacity [mAh]\n")
+    for cycle_id in cycle_dict.keys():
+        file_charge = open(full_basename + "_charge" + str(cycle_id) + output_file_extension, 'w')
+        file_discharge = open(full_basename + "_discharge" + str(cycle_id) + output_file_extension, 'w')
+        capacity_charge = cycle_dict[cycle_id]['Cycle charge capacity [mAh]']
+        capacity_discharge = cycle_dict[cycle_id]['Cycle discharge capacity [mAh]']
         if mass_g == None:
-            if 'Specific capacity [mAh/g]' in column_dict['record'].keys():
-                for i, line in enumerate(general_report.readlines()):
-                    if i < 2:
-                        pass
-                    else:
-                        cols = line.split("\t")
-                        if determine_row_type(cols, column_dict) == 'record':
-                            mAh = float(cols[colnum(column_dict['record']['Capacity [mAh]'])])
-                            mAh_per_g = float(cols[colnum(column_dict['record']['Specific capacity [mAh/g]'])])
-                            if mAh_per_g == 0.0:
-                                pass
-                            else:
-                                print "mAh =",str(mAh)
-                                print "mAh/g =",str(mAh_per_g)
-                                candidate_mass_g = mAh / mAh_per_g
-                                break
-                            # We've found a record, so let's break out of the for loop.
-                assert candidate_mass_g > 0
-                print "Data assumes mass of ",str(1000*candidate_mass_g),"mg."
-                print "Using this mass for calculations."
-        # Return to first byte of the file.
-        general_report.seek(0)
-        header_lines_to_skip = 3
-        step_type = None
-        output_delimiter = '\t'
-        header_comment_character = '#'
-        output_file_extension = '.dat'
-        cycle_summary_file = open(full_basename + "_all_cycle_summary" + output_file_extension, 'w')
-        grace_output_file = open(full_basename + "_grace_ascii.dat", 'w')
-        origin_output_file = open(full_basename + "_origin_columnar.csv", 'w')
-        if mass_g:
-            #TODO: avoid this gross duplication of code.
-            cycle_summary_file.write(header_comment_character + "CycleID" + output_delimiter + "charge capacity [mAh/g]" + output_delimiter + "discharge capacity [mAh/g]\n")
-        else:
-            cycle_summary_file.write(header_comment_character + "CycleID" + output_delimiter + "charge capacity [mAh]" + output_delimiter + "discharge capacity [mAh]\n")
-        all_cycles = []
-        for i, line in enumerate(general_report):
-            if i < header_lines_to_skip:
-                continue # don't process header lines
-            cols = line.split("\t")
-            row_type = determine_row_type(cols, column_dict)
-            if row_type == "cycle":
-                cycle_id = int(cols[colnum(column_dict[row_type]['Cycle ID'])])
-                print "Extracting cycle #",str(cycle_id)
-                capacity_charge = cols[colnum(column_dict[row_type]['Cycle charge capacity [mAh]'])]
-                capacity_discharge = cols[colnum(column_dict[row_type]['Cycle discharge capacity [mAh]'])]
-                if mass_g:
-                    specific_capacity_charge = float(capacity_charge)/(mass_g)
-                    specific_capacity_discharge = float(capacity_discharge)/(mass_g)
-                    cycle_summary_file.write(output_delimiter.join([str(cycle_id), str(specific_capacity_charge), str(specific_capacity_discharge)]) + "\n")
-                else:
-                    cycle_summary_file.write(output_delimiter.join([str(cycle_id), capacity_charge, capacity_charge]) + "\n")
-                grace_output_file.write("\n")
-            elif row_type == "step":
-                step_type = cols[colnum(column_dict[row_type]['Step type'])].strip()
-                #print step_type
-                grace_output_file.write("\n")
-                if step_type == "CC_Chg":
-                    filename = full_basename + "_charge" + str(cycle_id) + output_file_extension
-                    f = open(filename, 'w')
-                    f.write(header_comment_character + "mAh/g" + output_delimiter + "V\n")
-                elif step_type == "CC_DChg":
-                    filename = full_basename + "_discharge" + str(cycle_id) + output_file_extension
-                    f = open(filename, 'w')
-                    f.write(header_comment_character + "mAh/g" + output_delimiter + "V\n")
-                elif step_type == "Rest":
-                    pass
-                else:
-                    raise ValueError, "Unrecognized step type:" + step_type
-            elif row_type == "record":
-                if step_type == "CC_Chg" or step_type == "CC_DChg":
-                    V = cols[colnum(column_dict[row_type]['Voltage [V]'])]
-                    assert V != ""
-                    mAh = cols[colnum(column_dict[row_type]['Capacity [mAh]'])]
-                    assert mAh != ""
+            cycle_summary_file.write(output_delimiter.join([str(cycle_id), capacity_charge, capacity_discharge]) + "\n")
 
-                    if mass_g:
-                        mAh_per_g = str(float(mAh)/(mass_g))
-                        assert mAh_per_g != ""
-                    #print "line "+str(i)+" mAh: "+mAh
-                    if mass_g:
-                        f.write(mAh_per_g + output_delimiter + V + "\n")
-                        grace_output_file.write(mAh_per_g + " " + V + "\n")
-                    else:
-                        f.write(mAh + output_delimiter + V + "\n")
-                        grace_output_file.write(mAh + " " + V + "\n")
-                    #print V, mAh
-                elif step_type == "Rest":
-                    pass
-                else:
-                    raise ValueError, "Unrecognized step type:" + step_type
+            file_charge.write(header_comment_character + "mAh/g" + output_delimiter + "V\n")
+            for mAh, V in zip(cycle_dict[cycle_id]['charge']['mAh'],cycle_dict[cycle_id]['charge']['V']):
+                assert V != ""
+                assert mAh != ""
+                file_charge.write(mAh + output_delimiter + V + "\n")
+                grace_output_file.write(mAh + " " + V + "\n")
+            grace_output_file.write("\n")
+
+            file_discharge.write(header_comment_character + "mAh/g" + output_delimiter + "V\n")
+            for mAh, V in zip(cycle_dict[cycle_id]['discharge']['mAh'],cycle_dict[cycle_id]['discharge']['V']):
+                assert V != ""
+                assert mAh != ""
+                file_charge.write(mAh + output_delimiter + V + "\n")
+                grace_output_file.write(mAh + " " + V + "\n")
+            grace_output_file.write("\n")
+        else:
+            specific_capacity_charge = float(capacity_charge)/(mass_g)
+            specific_capacity_discharge = float(capacity_discharge)/(mass_g)
+            cycle_summary_file.write(output_delimiter.join([str(cycle_id), str(specific_capacity_charge), str(specific_capacity_discharge)]) + "\n")
+            file_charge.write(mAh_per_g + output_delimiter + V + "\n")
+            file_discharge.write(mAh_per_g + output_delimiter + V + "\n")
+            grace_output_file.write(mAh_per_g + " " + V + "\n")
+
+        if step_type == "CC_Chg":
+            f = open(filename, 'w')
+            f.write(header_comment_character + "mAh/g" + output_delimiter + "V\n")
+        elif step_type == "CC_DChg":
+            filename = full_basename + "_discharge" + str(cycle_id) + output_file_extension
+            f = open(filename, 'w')
+            f.write(header_comment_character + "mAh/g" + output_delimiter + "V\n")
+        elif step_type == "Rest":
+            pass
+        else:
+        if step_type == "CC_Chg" or step_type == "CC_DChg":
+            V = cols[colnum(column_dict[row_type]['Voltage [V]'])]
+            assert V != ""
+            mAh = cols[colnum(column_dict[row_type]['Capacity [mAh]'])]
+            assert mAh != ""
+
+            if mass_g:
+                mAh_per_g = str(float(mAh)/(mass_g))
+                assert mAh_per_g != ""
+            #print "line "+str(i)+" mAh: "+mAh
+            if mass_g:
+                f.write(mAh_per_g + output_delimiter + V + "\n")
+                grace_output_file.write(mAh_per_g + " " + V + "\n")
+            else:
+                f.write(mAh + output_delimiter + V + "\n")
+                grace_output_file.write(mAh + " " + V + "\n")
 
     cycle_summary_file.close()
     grace_output_file.close()
